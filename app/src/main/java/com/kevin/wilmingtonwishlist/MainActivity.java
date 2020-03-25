b2748a4ee2a0d2cc763e3205841a4cecdbd3cb3a
Kevin Todd <kmt8181@uncw.edu>
2020-03-25 10:50:21 -0400

    Set up Commit

--- app/src/main/java/com/kevin/wilmingtonwishlist/MainActivity.java
+++ app/src/main/java/com/kevin/wilmingtonwishlist/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.kevin.wilmingtonwishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kevin.wilmingtonwishlist.R;
 
 public class MainActivity extends AppCompatActivity {
 

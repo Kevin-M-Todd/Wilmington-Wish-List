cd19277cce8d9cacda5641363d8a8711c2869ded
Kevin Todd <kmt8181@uncw.edu>
2020-03-28 13:37:37 -0400

    Done: login authentication and sign up. Logout. Constraint Layouts.
    Yet to do:
    Clean up code.

--- app/src/main/java/com/kevin/wilmingtonwishlist/hub.java
+++ app/src/main/java/com/kevin/wilmingtonwishlist/hub.java
@@ -2,13 +2,24 @@ package com.kevin.wilmingtonwishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class hub extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+    }
+
+    public void logoutfunc(View view) {
+        mAuth.signOut();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
